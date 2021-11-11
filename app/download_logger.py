0502737889f8c113fd08a503b98d2a784e99adab
morykanm <morykanm@moravian.edu>
2021-11-10 20:10:24 -0500

    Created a file for the logger

---
+++ app/download_logger.py
@@ -0,0 +1,16 @@
+class Logger(object):
+    """
+    A logging class used in YoutubeDL when the user is downloading a video
+    """
+    
+    def debug(self, msg):
+        # print('debug', msg)
+        pass
+
+    def warning(self, msg):
+        # print('warning', msg)
+        pass
+
+    def error(self, msg):
+        # print('error', msg)
+        pass
